755b035cb2169035854e25cb126e57cedacbefe1
Peter Tribble <peter.tribble@gmail.com>
2025-01-30 21:33:21 +0000

    Sync patches for jdk 24+34

---
+++ jdk24/jdk-jdk-24-34/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
+--enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk23 \
+--with-native-debug-symbols=none \
+--with-toolchain-type=gcc \
+--disable-dtrace \
+--disable-warnings-as-errors \
+--with-source-date=current \
+DATE=/usr/gnu/bin/date \
+SORT=/usr/gnu/bin/sort \
+STRIP=/usr/gnu/bin/strip
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake product-images
