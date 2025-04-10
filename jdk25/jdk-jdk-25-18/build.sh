38e8ccaf495f0c7d25ca61216220157bd643865a
Peter Tribble <peter.tribble@gmail.com>
2025-04-10 21:37:58 +0100

    Sync patches for jdk 25+18

---
+++ jdk25/jdk-jdk-25-18/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
+--enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk24 \
+--with-native-debug-symbols=none \
+--with-toolchain-type=gcc \
+--disable-dtrace \
+--disable-warnings-as-errors \
+--with-source-date=current \
+DATE=/usr/gnu/bin/date \
+LOCALE=/bin/true \
+STRIP=/usr/gnu/bin/strip
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake product-images
